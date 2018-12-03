db418da21e2bf47ef59c7c2b93187d1fe55f6739
imransid <emailofimran1992@gmail.com>
2018-12-03 11:19:38 +0600

    ANTU VAI project update

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return jsonify({"about" : "Hello world"})
 
 
 if __name__ == '__main__':
